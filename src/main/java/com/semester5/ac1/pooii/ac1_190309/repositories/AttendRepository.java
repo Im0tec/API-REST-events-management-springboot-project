05d2d294ff5380efad5f3fc0aaad26360dab95b0
JBernecker <joh.bernecker@gmail.com>
2021-05-23 18:51:04 -0300

    AC2 NOTaTASK - Insert data

--- src/main/java/com/semester5/ac1/pooii/ac1_190309/repositories/AttendRepository.java
+++ src/main/java/com/semester5/ac1/pooii/ac1_190309/repositories/AttendRepository.java
@@ -1,3 +1,11 @@
+/*
+ * DUPLA:
+ *  
+ * Eduardo Campos Gonçalves - 190309
+ * Johanna Bernecker - 190737
+ * 
+ * TURMA: CP108LPIN2
+ */
 package com.semester5.ac1.pooii.ac1_190309.repositories;
 
 import java.util.List;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,14 +12,44 @@ INSERT INTO tb_event (name, description, place, start_date, end_date, start_time
 INSERT INTO tb_event (name, description, place, start_date, end_date, start_time, end_time, email) VALUES ('Sculpture Projects Münster', 'Evento de arte', 'Alemanha', '2022-07-10', '2022-08-01', '12:00:00', '20:00:00', 'SculptureMunster@gmail.com');
 
 --TB_PLACE
-INSERT INTO tb_place (name, address) VALUES ('Anzu', 'Indaiatuba');
+INSERT INTO tb_place (name, address) VALUES ('Bonvenon', 'Sao Paulo');
+INSERT INTO tb_place (name, address) VALUES ('Amarela Eventos', 'Indaiatuba');
+INSERT INTO tb_place (name, address) VALUES ('Thai Mai', 'Bangkok');
+INSERT INTO tb_place (name, address) VALUES ('Event Center', 'Los Angeles');
+INSERT INTO tb_place (name, address) VALUES ('Schawazergerter', 'Munique');
+INSERT INTO tb_place (name, address) VALUES ('Anime Zone', 'Campinas');
+INSERT INTO tb_place (name, address) VALUES ('Takihiroku', 'Osaka');
 
 --TB_BASEUSER
-INSERT INTO tb_baseuser (name, email) VALUES ('Eduardo', 'edu@edu');
-INSERT INTO tb_baseuser (name, email) VALUES ('Frate', 'frate@frate');
+INSERT INTO tb_baseuser (name, email) VALUES ('Eduardo', 'edu@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Imotec', 'Imotec@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('JB', 'jb@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Maro', 'maro@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Anonimo', 'nimo@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Random', 'rand@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Seila', 'sei@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Seilaa', 'seii@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Seilaaa', 'seiii@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Seilaaaa', 'seiiii@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Seilaaaaa', 'seiiiii@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Seilaaaaaa', 'seiiiiii@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Seilaaaaaaa', 'seiiiiiii@imail');
+INSERT INTO tb_baseuser (name, email) VALUES ('Seilaaaaaaaa', 'seiiiiiiii@imail');
 
 --TB_ADMIN
-INSERT INTO tb_admin (phone_number, baseuser_id) VALUES ('(11)983661517', 1);
+INSERT INTO tb_admin (phone_number, baseuser_id) VALUES ('(15)99999999', 1);
+INSERT INTO tb_admin (phone_number, baseuser_id) VALUES ('(15)99998888', 2);
+INSERT INTO tb_admin (phone_number, baseuser_id) VALUES ('(15)99997777', 3);
+INSERT INTO tb_admin (phone_number, baseuser_id) VALUES ('(15)99996666', 4);
+INSERT INTO tb_admin (phone_number, baseuser_id) VALUES ('(15)99995555', 5);
+INSERT INTO tb_admin (phone_number, baseuser_id) VALUES ('(15)99994444', 6);
+INSERT INTO tb_admin (phone_number, baseuser_id) VALUES ('(15)99993333', 7);
 
 --TB_ATTEND
-Insert INTO tb_attend (balance, baseuser_id) VALUES (120.50, 2);
+Insert INTO tb_attend (balance, baseuser_id) VALUES (100.10, 8);
+Insert INTO tb_attend (balance, baseuser_id) VALUES (110.20, 9);
+Insert INTO tb_attend (balance, baseuser_id) VALUES (120.30, 10);
+Insert INTO tb_attend (balance, baseuser_id) VALUES (130.40, 11);
+Insert INTO tb_attend (balance, baseuser_id) VALUES (140.50, 12);
+Insert INTO tb_attend (balance, baseuser_id) VALUES (140.60, 13);
+Insert INTO tb_attend (balance, baseuser_id) VALUES (150.60, 14);
